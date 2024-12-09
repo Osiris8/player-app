70b395d6e08c1b04ce866a09173c1f0727dca815
Osiris8 <miganosiris8@gmaaail.com>
2024-12-09 21:16:27 +0100

    Install daisy ui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import daisyui from "daisyui";
 
 export default {
   content: [
@@ -14,5 +15,22 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [daisyui],
+  daisyui: {
+    themes: [
+      {
+        playerTheme: {
+          primary: "#DC2626", // Rouge profond (tailwind.red.600)
+          secondary: "#111827", // Noir fumé (tailwind.gray.900)
+          accent: "#FACC15", // Doré (tailwind.amber.400)
+          neutral: "#1F2937", // Gris foncé (tailwind.gray.800)
+          "base-100": "#F3F4F6", // Gris clair (tailwind.gray.100)
+          info: "#60A5FA", // Bleu clair
+          success: "#16A34A", // Vert foncé
+          warning: "#F59E0B", // Orange doré
+          error: "#DC2626", // Rouge profond
+        },
+      },
+    ],
+  },
 } satisfies Config;
