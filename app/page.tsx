e02026312e75e40c8b15b0fcf64b981b480d351e
Osiris8 <miganosiris8@gmaaail.com>
2024-12-10 21:46:45 +0100

    Install lucide react and add footer component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
     <div data-theme="elegant">
       <Navbar />
+      <Footer />
     </div>
   );
 }
