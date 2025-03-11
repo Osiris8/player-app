7936e2f4d6abcb499b4e73d8e8d553d62190d46d
Osiris8 <miganosiris8@gmaaail.com>
2025-03-11 15:10:18 +0100

    Insert Navbar in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import localFont from "next/font/local";
 import "./globals.css";
 import { AuthProvider } from "./AuthProvider";
 import { EdgeStoreProvider } from "../lib/edgestore";
-
+import Navbar from "@/components/Navbar";
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -31,7 +31,10 @@ export default function RootLayout({
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
-          <EdgeStoreProvider>{children}</EdgeStoreProvider>
+          <EdgeStoreProvider>
+            <Navbar />
+            {children}
+          </EdgeStoreProvider>
         </body>
       </html>
     </AuthProvider>
