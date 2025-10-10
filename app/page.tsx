105c14a1bad033746a7bcdf48c913814c03d1e85
Osiris8 <miganosiris8@gmaaail.com>
2025-10-10 16:05:18 +0200

    update homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,16 @@
-"use client";
 import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
 
 import Card from "@/components/Card";
-import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
 
 export default function Home() {
-  const { isAuthenticated } = useKindeBrowserClient();
   return (
-    isAuthenticated && (
-      <div>
-        <Navbar />
-        <Hero />
-        <Card />
-        <Footer />
-      </div>
-    )
+    <div>
+      <Navbar />
+      <Hero />
+      <Card />
+      <Footer />
+    </div>
   );
 }
